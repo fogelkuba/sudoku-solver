e6aa98ce06bc7c0540e404118bff09cfed50ee66
fogelkuba <fogelkuba@gmail.com>
2021-07-06 22:52:10 +0200

    Working draft of initial project setup

---
+++ src/solver.test.ts
@@ -0,0 +1,9 @@
+import { Solver } from './solver'
+
+describe('Tests for solver class', () => {
+    test('Expect init test to work', () => {
+        const result = true
+
+        expect(result).toBe(true)
+    })
+})
